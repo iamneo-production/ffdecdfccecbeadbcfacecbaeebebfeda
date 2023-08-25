c5a2744e09c12b5449983181c9badec9acd341b2
Iamneo <techadmin@iamneo.ai>
2023-08-25 11:19:13 +0000

    Backup Commit

--- angularapp/src/app/app.component.ts
+++ angularapp/src/app/app.component.ts
@@ -64,31 +64,3 @@ this.totalCal += (parseInt(count) * calory)
 console.log(this.totalCal)
 }
 }
--------------------------------
-
-4.FILTER.PIPE.TS
-
-import { Pipe, PipeTransform } from "@angular/core";
-
-@Pipe({
-name: "filter"
-})
-export class FilterPipe implements PipeTransform {
-
-transform(value: any,filter:string){
-if (filter == ""){
-return value;
-}
-const foods = [];
-for(let v of value)
-{
-if(v.name.includes(filter))
-{
-foods.push(v);
-}
-}
-
-return foods;
-}
-
-} 
--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -1,20 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { FilterPipe } from "./filter.pipe";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+declarations: [
+AppComponent,
+FilterPipe
+],
+imports: [
+BrowserModule,
+AppRoutingModule,
+FormsModule
+],
+providers: [],
+bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { } 
