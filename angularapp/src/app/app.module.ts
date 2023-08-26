88360788cb15043dad674765cb76db2aa3669c5b
Iamneo <techadmin@iamneo.ai>
2023-08-26 12:05:44 +0000

    Backup Commit

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -4,12 +4,11 @@ import { BrowserModule } from "@angular/platform-browser";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { FilterPipe } from "./filter.pipe";
 
 @NgModule({
 declarations: [
 AppComponent,
-FilterPipe
+
 ],
 imports: [
 BrowserModule,
